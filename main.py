807958d3b048185cc7569352de6cc0738ac7cfd3
oscargomezq <oscar.gomezq@hotmail.com>
2020-04-18 15:09:36 +0400

    flask init

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
 # Testing
+
+# import pandas as pd
+from flask import Flask, jsonify, request
+# import pickle
+
+# load model
+# model = pickle.load(open('model.pkl','rb'))
+
+# app
+app = Flask(__name__)
+
+# routes
+@app.route('/', methods=['POST'])
+def index():
+	if request.method == "POST":
+		ret_dict = {"value": "YAY worked"}
+		return jsonify(**ret_dict)
+	# 	create_post(name,post)
+
+	# posts = get_posts()
+	# print("POST REQ")
+
+
+
+# def predict():
+#     # get data
+#     data = request.get_json(force=True)
+
+#     # convert data into dataframe
+#     data.update((x, [y]) for x, y in data.items())
+#     data_df = pd.DataFrame.from_dict(data)
+
+#     # predictions
+#     result = model.predict(data_df)
+
+#     # send back to browser
+#     output = {'results': int(result[0])}
+
+#     # return data
+#     return jsonify(results=output)
+
+if __name__ == '__main__':
+    app.run(port = 5000, debug=True)
